30e0d8062f636197ee8721fc3bbb34ec455c1a44
Isabela Mandressi <isaamandressi@gmail.com>
2023-01-26 17:13:09 -0300

    reentrega

--- scripts/seccionTienda.js
+++ scripts/seccionTienda.js
@@ -22,25 +22,86 @@ function mostrarProductos(productos) {
                     <h3 class="card-title">${prod.nombre}</h3>
                     <p class="card-text">$${prod.precio}</p>
                     <p class="card-text">${prod.cantidad}</p>
-                    <button class="agregarAlCarrito btn colorBoton""id="${prod.id}"> Agregar al carrito<button>
+                    <button class="agregarAlCarrito btn colorBoton" id="${prod.id}"> Agregar al carrito<button>
                 </div>
             </div>        
         `
         contenedorProductos.appendChild(card);
     })
-    const botonAgregarAlCarrito = document.querySelectorAll(`.agregarAlCarrito`);
+    const botonAgregarAlCarrito = document.querySelectorAll(".agregarAlCarrito");
     botonAgregarAlCarrito.forEach(btn => {
         btn.addEventListener("click", (e) => agregarAlCarrito(e, productos))
     })
 
 }
 
-
+let carrito = [];
 
 function agregarAlCarrito(e, prods) {
-    console.log(prods)
-    console.log(e.target.id)
     const productoElegido = prods.find(el => el.id === parseInt(e.target.id));
     console.log(productoElegido)
-}
+    if(productoElegido){
+        productoElegido.cantidad++
+        carrito.push(productoElegido)
+        localStorage.setItem("carrito", JSON.stringify(carrito))
+    }
+    else{
+        carrito.push(productoElegido);
+        localStorage.setItem("carrito", JSON.stringify(carrito))
+    }
+    }
 
+    let productosCarrito = JSON.parse(localStorage.getItem("carrito", JSON.stringify(carrito)))
+   
+
+    function mostrarCarrito(carrito) {
+        carrito.forEach((productosCarrito) => {
+            const card = document.createElement("div");
+            card.innerHTML = `
+                    <div class="card unProductoCarrito">
+                        <img src="../${productosCarrito.img}" class="card-img-top imgProductos alt="${productosCarrito.nombre}">
+                        <div class="card body datosProductoCarrito">
+                            <h3 class="card-title">${productosCarrito.nombre}</h3>
+                            <p class="card-text">$${productosCarrito.precio}</p>
+                            <p class="card-text">${productosCarrito.cantidad}</p>
+                            <button class="botonEliminar btn colorBoton" id="${productosCarrito.id}"> Eliminar del Carrito<button>
+                        </div>
+                    </div>        
+                `
+            carritoDeCompras.appendChild(card);
+        })
+        const botonEliminarDelCarrito = document.querySelectorAll(`.botonEliminar`);
+        botonEliminarDelCarrito.forEach(btn => {
+            btn.addEventListener("click", (e) => eliminarDelCarrito(e, carrito))
+        })
+        calcularTotal();
+    }
+    console.log(mostrarCarrito)
+    
+    function calcularTotal() {
+        let totalCompra = 0;
+        carrito.forEach((productosCarrito) => {
+            totalCompra += productosCarrito.precio * productosCarrito.cantidad;
+        })
+        total.innerHTML = `$${totalCompra}`;
+        console.log(totalCompra)
+    }
+    
+    
+    
+    function eliminarDelCarrito(e, carrito) {
+        const index = carrito.findIndex(el => el.id === parseInt(e.target.id))
+        carrito.splice(index, 1);
+        localStorage.seItem("carrito", JSON.stringify(carrito))
+        mostrarCarrito();
+    }
+    
+    //otonVaciar.addEventListener("click", eliminarCarrito);
+    
+    function eliminarCarrito() {
+        carrito = [];
+        localStorage.clear("carrito", JSON.stringify(carrito))
+        mostrarCarrito()
+    }
+    
+    console.log(productosCarrito)
